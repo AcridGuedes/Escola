3fe8bf69799e25dad80c35bff53137b72df0f1d8
AcridGuedes <53222754+AcridGuedes@users.noreply.github.com>
2022-11-08 18:12:25 +0000

    Update Calculadora.c

--- Calculadora.c
+++ Calculadora.c
@@ -12,31 +12,35 @@ int main() {
     n2=0;
     cout<<"1-Soma\n";
     cout<<"2-Subtração\n";
-    cout<<"3-Multiplicação\n";
+    cout<<"3-Multiplicação";
     cout<<"4-Divisão\n";
     cout<<"5-Potencia\n";
+    cout<<"6-Raiz quadradad\n";
     cout<<"Escolha uma operação -> ";
     cin>>op;
     cout<<"Escolha o primeiro número a utilizar -> ";
-    cin>>n1;
-      cout<<"Escolha o segundo número a utilizar -> \n";
+  cin>>n1;
+      cout<<"Escolha o segundo número a utilizar -> ";
       cin>>n2;
     switch (op){
     case 1:
-        cout<<""<<n1+n2;
+        cout<<"O resultado é -> "<<n1+n2;
         break;
     case 2:
-        cout<<""<<n1-n2;
+        cout<<"O resultado é -> "<<n1-n2;
         break;
     case 3:
-        cout<<""<<n1*n2;
+        cout<<"O resultado é -> "<<n1*n2;
         break;
     case 4:
-        cout<<""<<n1/n2;
+        cout<<"O resultado é -> "<<n1/n2;
         break;
     case 5:
         cout<<"O resultado é -> "<<pow(n1,n2);
         break;
+    case 6:
+        cout<<"O resultado é ->"<<sqrt(n1);
+    
     }
     
     
