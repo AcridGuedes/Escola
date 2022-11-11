bfa224dc6e5ed6b5142ba3950069f9ce522dde54
AcridGuedes <53222754+AcridGuedes@users.noreply.github.com>
2022-11-11 23:35:53 +0000

    Update Calculadora_Boa.cpp
    Changed everithing lol

--- Calculadora_Boa.cpp
+++ Calculadora_Boa.cpp
@@ -1,96 +1,87 @@
 #include <iostream>
 #include <math.h>
 
-int main(){
-	while (true) {
-		using namespace std;
+using namespace std;
+int main()
+{
+	while(true){
+	int choice, op;
+	float nums[5], total;
+	cout << "Welcome to the worst calculator. Have fun!!\n";
+	cout << "1-Sum(Working)\n";
+	cout << "2-Subtraction(Work in progress)\n";
+	cout << "3-Multiplication(Working)\n";
+	cout << "4-Division(Working)\n";
+	cout << "5-Avarege(Working)\n\n";
+	cout<< "Choose the operation.\n> ";
+	cin >> op;
+	cout << "How many numbers would you like to use?\n> ";
+	cin >> choice;
 
-		int op, nx, ny;
-		float n1, n2;
+	switch (op) {
+	case 1:
+		total = 0;
+		for (int i = 0; i < choice; i++) {
+			cout << "Choose the numbers!\n> ";
+			cin >> nums[i];
 
-		cout << "\nBem vindo a calculadora do Ivo, Gui e Joao!\n\n";
-		cout << "1-Soma\n";
-		cout << "2-Subtracao\n";
-		cout <<"3-Multiplicacao\n";
-		cout << "4-Divisão\n";
-		cout << "5-Potencia\n";
-		cout << "6-Raiz Quadrada\n";
-		cout << "7-Media\n";
-		cout << "8-Resto da divisao\n\n";
-		cout << "Para comecar escolha uma operacao -> ";
-		cin >> op;
-				switch (op) {
-		case 1:
-			cout << "Escolha os numeros a utilizar\n\n";
-			cout << "1-> ";
-			cin >> n1;
-			cout << "2-> ";
-			cin >> n2;
-			cout << "O resultado e -> " << n1 + n2;
-			break;
-		case 2:
-			cout << "Escolha os numeros a utilizar\n\n";
-			cout << "1-> ";
-			cin >> n1;
-			cout << "2-> ";
-			cin >> n2;
-			cout << "O resultado e -> " << n1 - n2;
-			break;
-		case 3:
-			cout << "Escolha os numeros a utilizar\n\n";
-			cout << "1-> ";
-			cin >> n1;
-			cout << "2-> ";
-			cin >> n2;
-			cout << "O resultado e -> " << n1 * n2;
-			break;
-		case 4:
-			cout << "Escolha os numeros a utilizar\n\n";
-			cout << "1-> ";
-			cin >> n1;
-			cout << "2-> ";
-			cin >> n2;
-			cout << "O resultado e -> " << n1 / n2;
-			break;
-		case 5:
-			cout << "Escolha os numeros a utilizar\n\n";
-			cout << "1-> ";
-			cin >> n1;
-			cout << "2-> ";
-			cin >> n2;
-			cout << "O resultado e -> " << pow(n1, n2);
-			break;
-		case 6:
-			cout << "Escolha os numeros a utilizar\n\n";
-			cout << "1-> ";
-			cin >> n1;
-			cout << "2-> ";
-			cin >> n2;
-			cout << "O resultado e -> " << sqrt(n1) << ";" << sqrt(n2);
-			break;
-		case 7:
-			cout << "Escolha os numeros a utilizar\n\n";
-			cout << "1-> ";
-			cin >> n1;
-			cout << "2-> ";
-			cin >> n2;
-			cout << "O resultado e -> " << (n1 + n2) / 2;
-			break;
-		case 8:
-			cout << "Escolha os numeros a utilizar\n\n";
-			cout << "1-> ";
-			cin >> nx;
-			cout << "2-> ";
-			cin >> ny;
-			cout << "O resultado e -> " << nx % ny;
-			break;
 		}
-
-				cout << "\n\nObrigado por usar esta calculadora mal programada <3!\n\n";
-				cout << "\n<=========================================================>\n\n";
+		for (int x = 0; x < choice; x++){
+			total = total + nums[x];
 	}
 
-		return 0;
-	
+		cout << "O total e " << total << "\n\n";
+		break;
+	case 2:
+		total = 0;
+		for (int i = 0; i < choice; i++) {
+			cout << "Choose the numbers!\n> ";
+			cin >> nums[i];
 
+		}
+		for (int x = 0; x < choice; x++) {
+			total = total - nums[x];
+			cout << total;
+		}
+		cout << "O total e " << total << "\n\n";
+	case 3:
+		total = 1;
+		for (int i = 0; i < choice; i++) {
+			cout << "Choose the numbers!\n> ";
+			cin >> nums[i];
+
+		}
+		for (int x = 0; x < choice; x++){
+			total = total * nums[x];
+	}
+		
+		cout << "O total e " << total << "\n\n";
+		break;
+	case 4:
+		total = 0;
+		for (int i = 0; i < choice; i++) {
+			cout << "Choose the numbers!\n> ";
+			cin >> nums[i];
+
+		}
+		cout << "O resultado e > " << nums[0] / nums[1]<<"\n\n";
+
+		break;
+	case 5:
+		total = 0;
+		for (int i = 0; i < choice; i++) {
+			cout << "Choose the numbers!\n> ";
+			cin >> nums[i];
+
+		}
+		for (int x = 0; x < choice; x++)
+			total = nums[0] + nums[1];
+		cout << "The result is " << total / choice<<"\n\n";
+
+		break;
+	
+	}
+	}
+	 return 0;
+ 
 }
